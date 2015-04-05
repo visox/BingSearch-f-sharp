d65613e7e319807fa2f6bcb3df752dbc218e2fa2
michal <viso.laci@gmail.com>
2015-04-05 02:12:36 +0200

    initial commit, contains 2 programms. One c# library project for one large class that is used in second f# project.

---
+++ BingAPI/BingSearch.fs
@@ -0,0 +1,22 @@
+﻿module BingSearch
+
+open System
+open System.Net
+open System.Xml
+open System.Data.Services.Client
+
+open BingSearchContainer
+
+let private bingUrl = "https://api.datamarket.azure.com/Bing/Search"
+let private key = "add from ...https://datamarket.azure.com/..."
+
+let getBingResult query = 
+    let bingContainer = new BingSearchContainer(new Uri(bingUrl)) 
+    bingContainer.Credentials <- new NetworkCredential(key, key)
+    let webQuery = bingContainer.Web(query, null, null, null, null, Nullable<float>(), Nullable<float>(), null)
+    webQuery.Execute() |> ignore
+    [
+        let enumerator = webQuery.GetEnumerator()
+        while enumerator.MoveNext() do
+            yield enumerator.Current.Url 
+    ]
